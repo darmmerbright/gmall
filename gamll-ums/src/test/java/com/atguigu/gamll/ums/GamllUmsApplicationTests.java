4e2f261f7b7933d313d5307d90b9092ac49551d7
darmmerbright <2390529747@qq.com>
2020-10-28 08:57:26 +0800

    创建后台工程

---
+++ gamll-ums/src/test/java/com/atguigu/gamll/ums/GamllUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gamll.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamllUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
